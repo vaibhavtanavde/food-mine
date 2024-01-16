28d28627515c95e6fd2358f776dc40541f7e3014
vaibhavtanavde <vaibhavt.work@gmail.com>
2024-01-16 23:00:42 +0100

    prototype & npm link implementation

---
+++ code-visual/app.js
@@ -0,0 +1,8 @@
+const yourPackage = require('your-package-name');
+
+// Specify your project-specific paths
+const baseFolderPath = '..//foodmine-course//frontend//src';
+const exampleSpecPath = '..//testcases//example_spec.js';
+
+// Use the functionality from your package
+yourPackage.startServer(baseFolderPath, exampleSpecPath);
