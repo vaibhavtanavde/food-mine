89ba9c22ea5c48ae8ac63d33fd228ccea8cbbc4f
vaibhavtanavde <vaibhavt.work@gmail.com>
2024-01-21 12:20:54 +0100

    Package npm link

--- code-visual/app.js
+++ code-visual/app.js
@@ -1,8 +1,6 @@
-const yourPackage = require('your-package-name');
-
+const yourPackage = require('toolpackage');
 // Specify your project-specific paths
 const baseFolderPath = '..//foodmine-course//frontend//src';
 const exampleSpecPath = '..//testcases//example_spec.js';
-
 // Use the functionality from your package
 yourPackage.startServer(baseFolderPath, exampleSpecPath);
